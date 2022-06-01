131e819680ed1d003d567756f639a229be32238f
diogodvs <dvsdevx@gmail.com>
2022-06-01 11:02:56 -0300

    Novo exercicio usando Interface

---
+++ src/figurasGeometricasInterface/CalculosGeometricos.java
@@ -0,0 +1,5 @@
+package figurasGeometricasInterface;
+
+public class CalculosGeometricos {
+
+}
---
+++ src/figurasGeometricasInterface/Main.java
@@ -0,0 +1,10 @@
+package figurasGeometricasInterface;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
